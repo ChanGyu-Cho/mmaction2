c35228f5f81b7526e7719a42b1d1fea1067987ab
조규찬 <qqppqqppqp35@gmail.com>
2025-07-13 14:50:07 +0900

    normalize, Improved verification data performance
    학습 epoch를 16→10으로 단축 (max_epochs=10).
    
    학습 스케줄을 CosineAnnealing→MultiStepLR(마일스톤 [3,6], γ=0.1)로 변경.
    
    학습률을 0.01→0.005로 낮추고 weight_decay를 0.0001로 수정.
    
    optimizer에서 Nesterov momentum 유지.
    
    Mixed-Precision(fp16) 설정 유지.
    
    EarlyStoppingHook 모니터링을 val/top1_acc, patience=5, min_delta=1e-3로 설정.

--- configs/skeleton/stgcnpp/my_stgcnpp.py
+++ configs/skeleton/stgcnpp/my_stgcnpp.py
@@ -1,5 +1,8 @@
 _base_ = '../../_base_/default_runtime.py'
 
+# FP16 Mixed-Precision 설정
+fp16 = dict(type='Fp16OptimizerHook', loss_scale='dynamic')
+
 load_from = r"D:\mmaction2\checkpoints\stgcnpp_8xb16-joint-u100-80e_ntu60-xsub-keypoint-2d_20221228-86e1e77a.pth"
 
 model = dict(
@@ -9,11 +12,19 @@ model = dict(
         gcn_adaptive='init',
         gcn_with_res=True,
         tcn_type='mstcn',
-        graph_cfg=dict(layout='coco', mode='spatial')),
-    cls_head=dict(type='GCNHead', num_classes=2, in_channels=256))
+        graph_cfg=dict(layout='coco', mode='spatial')
+    ),
+    cls_head=dict(
+        type='GCNHead',
+        num_classes=2,
+        in_channels=256,
+        dropout=0.7 # Dropout 비율 조정
+    )
+)
 
 dataset_type = 'PoseDataset'
 ann_file = r"D:\golfDataset\스포츠 사람 동작 영상(골프)\Training\Public\male\train\crop_pkl\skeleton_dataset_90_10.pkl"
+
 train_pipeline = [
     dict(type='PreNormalize2D'),
     dict(type='GenSkeFeat', dataset='coco', feats=['j']),
@@ -25,8 +36,7 @@ train_pipeline = [
 val_pipeline = [
     dict(type='PreNormalize2D'),
     dict(type='GenSkeFeat', dataset='coco', feats=['j']),
-    dict(
-        type='UniformSampleFrames', clip_len=100, num_clips=1, test_mode=True),
+    dict(type='UniformSampleFrames', clip_len=100, num_clips=1, test_mode=True),
     dict(type='PoseDecode'),
     dict(type='FormatGCNInput', num_person=2),
     dict(type='PackActionInputs')
@@ -34,9 +44,7 @@ val_pipeline = [
 test_pipeline = [
     dict(type='PreNormalize2D'),
     dict(type='GenSkeFeat', dataset='coco', feats=['j']),
-    dict(
-        type='UniformSampleFrames', clip_len=100, num_clips=10,
-        test_mode=True),
+    dict(type='UniformSampleFrames', clip_len=100, num_clips=10, test_mode=True),
     dict(type='PoseDecode'),
     dict(type='FormatGCNInput', num_person=2),
     dict(type='PackActionInputs')
@@ -49,12 +57,15 @@ train_dataloader = dict(
     sampler=dict(type='DefaultSampler', shuffle=True),
     dataset=dict(
         type='RepeatDataset',
-        times=5,
+        times=1,
         dataset=dict(
             type=dataset_type,
             ann_file=ann_file,
             pipeline=train_pipeline,
-            split='xsub_train')))
+            split='xsub_train'
+        )
+    )
+)
 val_dataloader = dict(
     batch_size=16,
     num_workers=2,
@@ -65,7 +76,9 @@ val_dataloader = dict(
         ann_file=ann_file,
         pipeline=val_pipeline,
         split='xsub_val',
-        test_mode=True))
+        test_mode=True
+    )
+)
 test_dataloader = dict(
     batch_size=1,
     num_workers=2,
@@ -76,34 +89,53 @@ test_dataloader = dict(
         ann_file=ann_file,
         pipeline=test_pipeline,
         split='xsub_val',
-        test_mode=True))
+        test_mode=True
+    )
+)
 
-val_evaluator = [dict(type='AccMetric')]
+val_evaluator = [
+    dict(type='AccMetric'),
+]
 test_evaluator = val_evaluator
 
-train_cfg = dict(
-    type='EpochBasedTrainLoop', max_epochs=16, val_begin=1, val_interval=1)
+# Training schedule 조정으로 과적합 억제
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=8, val_begin=1, val_interval=1)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
 param_scheduler = [
     dict(
-        type='CosineAnnealingLR',
-        eta_min=0,
-        T_max=16,
+        type='MultiStepLR',
+        begin=0,
+        end=10,
         by_epoch=True,
-        convert_to_iter_based=True)
+        milestones=[3, 6],
+        gamma=0.1
+    )
 ]
 
 optim_wrapper = dict(
     optimizer=dict(
-        type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005, nesterov=True),
-    clip_grad=dict(max_norm=5, norm_type=2))
+        type='SGD',
+        lr=0.005,
+        momentum=0.9,
+        weight_decay=0.0001,
+        nesterov=True
+    )
+)
 
-default_hooks = dict(checkpoint=dict(interval=1), logger=dict(interval=100))
+default_hooks = dict(
+    checkpoint=dict(interval=1),
+    logger=dict(interval=100)
+)
 
-# Default setting for scaling LR automatically
-#   - `enable` means enable scaling LR automatically
-#       or not by default.
-#   - `base_batch_size` = (8 GPUs) x (16 samples per GPU).
+custom_hooks = [
+    dict(
+        type='EarlyStoppingHook',
+        monitor='val/top1_acc',
+        patience=5,
+        min_delta=1e-3
+    )
+]
+
 auto_scale_lr = dict(enable=False, base_batch_size=128)
